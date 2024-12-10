aa379be45386e72bad4c7ccfb116d664cdc93a3d
Ian Kabaka <iankabaka1@gmail.com>
2024-12-10 13:40:38 +0300

    passing tests for sql_queries python

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+  ("Mr. Chocolate", 12, "M", "brown", "friendly", 1),
+  ("Rowdy", 8, "M", "black", "aggressive", 1),
+  ("Tabitha", 4, "F", "white", "curious", 0),
+  ("Sergeant Brown", 15, "M", "gray", "protective", 1),
+  ("Melissa", 6, "F", "golden", "playful", 1),
+  ("Grinch", 10, "M", "dark brown", "grumpy", 0),
+  ("Wendy", 7, "F", "honey", "gentle", 1),
+  (NULL, 3, "F", "cream", "shy", 0);
